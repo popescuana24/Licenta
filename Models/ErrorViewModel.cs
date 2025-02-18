110ab9722be8c93bdf6f3683fef2928d91f76841
Ana Popescu <popescuana22@stud.ase.ro>
2025-02-18 20:15:43 +0200

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ClothingWebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
